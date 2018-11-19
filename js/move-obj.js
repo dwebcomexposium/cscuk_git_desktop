fe1d1a24c3acda0426c4d17812d7d34d3a6ed5a5
Thomascxpm <33687666+ThomasRny@users.noreply.github.com>
2018-11-19 16:48:57 +0100

    Update move-obj.js

--- js/move-obj.js
+++ js/move-obj.js
@@ -1,6 +1,6 @@
 $('.news-home .inside').append($('.link-view-all'));
 
-$( '#overlay.is-open' ).on("click", function(){
+$('#overlay.is-open').on("click", function(){
   $('#overlay').removeClass('is-open');
   $('.nav-general').removeClass('mn-visible');
 });
